0f24c9103a60ba3f8591a2885d528871f038483f
DeclanCassells <declan.cassells@gmail.com>
2013-09-25 13:02:36 +0100

    First

---
+++ _documents/js/area.js
@@ -0,0 +1,13 @@
+var unitID = "area";
+var defaultUnitIndex = 11;
+
+var units = [
+  ["acres",                      4046.8564224],
+  ["hectares",                   1e4],
+  ["square feet",         0.09290304],
+  ["square kilometers",          1e6],
+  ["square meters",              1],
+  ["square miles",               2589988.110336],
+  ["square yards",               0.83612736],
+
+]; 
